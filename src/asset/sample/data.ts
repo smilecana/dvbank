e0800d6b1d5f3a7bf20b23dcebdd7f4357bcbe15
smilecana <smilecana@gmail.com>
2020-03-17 13:04:55 -0300

    Change Directory structure

---
+++ src/asset/sample/data.ts
@@ -0,0 +1,63 @@
+export default {
+    menus: [
+        {
+            "id": 1,
+            "title": "Home",
+            "link": "/home",
+            "icon": "home",
+            "open": false,
+            "items": []
+        },
+        {
+            "id": 2,
+            "title": "Accounts",
+            "link": "",
+            "icon": "account_balance",
+            "open": true,
+            "items": [
+                {
+                    "id": 1,
+                    "title": "Summary",
+                    "link": "/summary"
+                },
+                {
+                    "id": 2,
+                    "title": "Account Details",
+                    "link": "/details"
+                },
+                {
+                    "id": 3,
+                    "title": "Account Document",
+                    "link": "/document"
+                }
+            ]
+        },
+        {
+            "id": 3,
+            "title": "Transfers",
+            "link": "/transfer",
+            "icon": "payment",
+            "open": true,
+            "items": [
+                {
+                    "id": 1,
+                    "title": "Transfer",
+                    "link": "/transfer"
+                },
+                {
+                    "id": 2,
+                    "title": "Transfers History",
+                    "link": "/transfers-history"
+                }
+            ]
+        },
+        {
+            "id": 4,
+            "title": "Bills",
+            "link": "/bill",
+            "icon": "payment",
+            "open": true,
+            "items": []
+        }
+    ]
+}
